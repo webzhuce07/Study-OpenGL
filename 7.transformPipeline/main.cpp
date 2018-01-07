4e92061d8b0da1c7652b588455c828b0e1bbb187
webzhuce07 <lajiwebzhuce@163.com>
2018-01-07 20:04:58 +0800

    3D Viewing and lighting model
    3D Viewing and lighting model

---
+++ 7.transformPipeline/main.cpp
@@ -0,0 +1,20 @@
+#include <QApplication>
+#include <iostream>
+#include "myQGLWidget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    if (!QGLFormat::hasOpenGL()) {
+        std::cerr << "This system has no OpenGL support" << std::endl;
+        return 1;
+    }
+
+    MyQGLWidget widget;
+    widget.setWindowTitle(QObject::tr("Texture Map"));
+	widget.resize(600, 600);
+	widget.show();
+
+    return app.exec();
+}
