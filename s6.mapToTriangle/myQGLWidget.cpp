546039c547f0e5fe0197ede4d2b3cc1b00d154f3
webzhuce07 <lajiwebzhuce@163.com>
2018-03-11 20:28:15 +0800

    s6. texture map of  triangle
    s6. texture map of  triangle

--- s6.mapToTriangle/myQGLWidget.cpp
+++ s6.mapToTriangle/myQGLWidget.cpp
@@ -12,6 +12,7 @@ void MyQGLWidget::initializeGL()
 {
     initializeOpenGLFunctions();
     glEnable(GL_DEPTH_TEST);
+    loadGLTextures();
     initShader();
 }
 
@@ -20,94 +21,72 @@ void MyQGLWidget::resizeGL(int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void MyQGLWidget::loadGLTextures()
+{
+    QImage tex, buf;
+    if(!buf.load(":/new/img/texture.jpg")){
+        QImage dummy(128, 128, QImage::Format_RGB32);
+        dummy.fill(Qt::green);
+        buf = dummy;
+    }
+    tex = QGLWidget::convertToGLFormat(buf);
+    glGenTextures(1, &texture[0]);
+    glTexImage2D( GL_TEXTURE_2D, 0, 3, tex.width(), tex.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, tex.bits() );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
+}
+
 void MyQGLWidget::paintGL()
 {
-    glEnable(GL_DEPTH_TEST);
     glClear(GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 1.0);
-    //glBindVertexArray(VAOs[Triangles]);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, NULL);
-    //glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+    glBindVertexArray(VAOs[Triangles]);
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 }
 
 void MyQGLWidget::initShader()
 {
     glGenVertexArrays(NumVAOs, VAOs);
     glBindVertexArray(VAOs[Triangles]);
-    // create a cube
-    //    v6----- v5
-    //   /|      /|
-    //  v1------v0|
-    //  | |     | |
-    //  | |v7---|-|v4
-    //  |/      |/
-    //  v2------v3
     GLfloat vertices[] = {
-        // position                 // color
-        0.5,  0.5,  0.5,     1.0,  1.0,  1.0,  // v0 White
-        -0.5,  0.5,  0.5,    1.0,  0.0,  1.0,  // v1 Magenta
-        -0.5, -0.5,  0.5,    1.0,  0.0,  0.0,  // v2 Red
-        0.5, -0.5,  0.5,     1.0,  1.0,  0.0,  // v3 Yellow
-        0.5, -0.5, -0.5,     0.0,  1.0,  0.0,  // v4 Green
-        0.5,  0.5, -0.5,     0.0,  1.0,  1.0,  // v5 Cyan
-        -0.5,  0.5, -0.5,    0.0,  0.0,  1.0,  // v6 Blue
-        -0.5, -0.5, -0.5,    0.0,  0.0,  0.0   // v7 Black
+        // 位置                 // 纹理坐标
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f,   // 左下
+         0.0f,  0.5f, 0.0f,  0.5f, 1.0f,   // 顶部
     };
-
-    //vertex index
-    GLushort indices[36] = {
-        0, 1, 2,   0, 2, 3,    // front
-
-        0, 3, 4,   0, 4, 5,    // right
-
-        0, 5, 6,   0, 6, 1,    // up
-
-        1, 6, 7,   1, 7, 2,    // right
-
-        7, 4, 3,   7, 3, 2,    // down
-
-        4, 7, 6,   4, 6, 5     // back
-
-     };
-
 
     glGenBuffers(NumBuffers, Buffers);
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(vPosition);
-    glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(vPosition, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(vColor);
-    glVertexAttribPointer(vColor, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
-
-    GLuint  indexBufferID;
-    glGenBuffers(1, &indexBufferID);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(vColor, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
 
     const char*  vertexShaderCode =
         "#version 430 \n"
         ""
         "layout(location = 0) in vec3 vPosition;"
-        "layout(location = 1) in vec3 color;"
-        "out vec3 myColor;"
+        "layout(location = 1) in vec2 textureCoord;"
+        "out vec2 myTextureCoord;"
         ""
         "void main()"
         "{"
-		"   mat4 rotatey = mat4(0.5, 0.0, 0.866, 0.0,   0.0,1.0,0.0,0.0, -0.866,0.0,0.5,0.0,  0.0,0.0, 0.0,1.0);"
-		"   mat4 rotatex = mat4(1.0, 0.0, 0.0, 0.0,   0.0,0.707,-0.707,0.0, 0.0,0.707,0.707,0.0,  0.0,0.0, 0.0,1.0);"
-		"   gl_Position =  rotatex *rotatey* vec4(vPosition, 1.0);"
-        "   myColor = color;"
+        "   gl_Position = vec4(vPosition, 1.0);"
+        "   myTextureCoord = textureCoord;"
         "}";
 
     const char* fragmentShaderCode =
         "#version 430 \r \n"
         ""
-        "in vec3 myColor;"
+        "in vec2 myTextureCoord;"
         "out vec4 fColor;"
+        "uniform sampler2D myTexture;"
         ""
         "void main()"
         "{"
-        "   fColor = vec4(myColor, 1.0f);"
+        "   fColor = texture(myTexture, myTextureCoord);"
         "}";
     GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
     GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
@@ -124,38 +103,39 @@ void MyQGLWidget::initShader()
     GLint compiled;
     glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &compiled );
     if (!compiled)
-	{ 
-		std::cout<<"vertexShaderID";
+    {
+        std::cout<<"vertexShaderID";
         GLsizei len;
         glGetShaderiv( vertexShaderID, GL_INFO_LOG_LENGTH, &len );
+
         GLchar* log = new GLchar[len+1];
         glGetShaderInfoLog( vertexShaderID, len, &len, log );
         std::cerr << "Shader compilation failed: " << log << std::endl;
         delete [] log;
-	}
-
+    }
     glCompileShader(fragmentShaderID);
 
     glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &compiled );
-    if (!compiled) 
-	{ 
-		std::cout<<"fragmentShaderID";
+    if (!compiled)
+    {
+        std::cout<<"fragmentShaderID";
         GLsizei len;
         glGetShaderiv( fragmentShaderID, GL_INFO_LOG_LENGTH, &len );
         GLchar* log = new GLchar[len+1];
         glGetShaderInfoLog( fragmentShaderID, len, &len, log );
         std::cerr << "Shader compilation failed: " << log << std::endl;
         delete [] log;
-    }
+     }
 
     GLuint programID = glCreateProgram();
     glAttachShader(programID, vertexShaderID);
     glAttachShader(programID, fragmentShaderID);
 
     glLinkProgram(programID);
+
     GLint linked;
     glGetProgramiv( programID, GL_LINK_STATUS, &linked );
-    if (!linked) 
-		std::cout<<"Error";
+    if (!linked)
+        std::cout<<"Error";
     glUseProgram(programID);
 }
